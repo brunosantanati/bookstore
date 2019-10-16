c82f90e5a4e67e738f1e044b41ef3809ab197035
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-10-16 18:45:01 -0300

    Changed the CSV URL for the file in my GitHub. Also provided a comment which explains a Node.js alternative.

--- src/br.com.casadocodigo.http/br/com/casadocodigo/http/Books.java
+++ src/br.com.casadocodigo.http/br/com/casadocodigo/http/Books.java
@@ -13,7 +13,14 @@ public class Books {
     try {
       String csv = HttpClient.newHttpClient()
       .send(HttpRequest.newBuilder()
-        .uri(new URI("https://turini.github.io/livro-java-9/books.csv"))
+        .uri(new URI("https://raw.githubusercontent.com/brunosantanati/bookstore/master/csv-example/books.csv"))
+        //.uri(new URI("https://turini.github.io/livro-java-9/books.csv"))
+
+        /*It's possible to consume the CSV file from a local Node.js server.
+        I developed the Node.js application to run all things locally. 
+        URL for downloading: https://github.com/brunosantanati/csv-downloader */
+        //.uri(new URI("http://127.0.0.1:3000/books.csv"))
+
         .GET().build(),
       //HttpResponse.BodyHandler.asString()).body(); //Java 9
       HttpResponse.BodyHandlers.ofString()).body(); //Java 11
@@ -31,7 +38,7 @@ public class Books {
     String[] split = line.split(",");  
     String name = split[0];
     String author = split[2];
-    Category category = Category.valueOf(split[3]);
+    Category category = Category.valueOf(split[3].trim());
     return new Book(name, author, category);
   }
 
